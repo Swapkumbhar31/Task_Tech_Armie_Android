077c4cd6f46afed1e41ca751857a7ae740a4a69f
BhautikPethani <c0854487@mylambton.ca>
2022-06-16 03:44:07 -0400

    Actionbar has been removed

--- app/src/main/java/ca/lambton/task_tech_armie_android/MainActivity.java
+++ app/src/main/java/ca/lambton/task_tech_armie_android/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-
         taskRoomDB = TaskRoomDB.getInstance(this);
         taskRoomDB.taskDAO().getAllTasks();
     }
