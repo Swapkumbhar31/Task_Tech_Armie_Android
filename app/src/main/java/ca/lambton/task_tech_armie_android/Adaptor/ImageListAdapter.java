af62c7cdb6f4f09fec1890cdea96fadb729e98f9
Swapkumbhar31 <swapkumbhar31@gmail.com>
2022-06-17 12:22:54 -0400

    task image added

--- app/src/main/java/ca/lambton/task_tech_armie_android/Adaptor/ImageListAdapter.java
+++ app/src/main/java/ca/lambton/task_tech_armie_android/Adaptor/ImageListAdapter.java
@@ -1,44 +1,61 @@
 package ca.lambton.task_tech_armie_android.Adaptor;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import ca.lambton.task_tech_armie_android.R;
 
-public class ImageListAdapter extends BaseAdapter {
+public class ImageListAdapter extends RecyclerView.Adapter<ImageItemViewHolder> {
 
     Context context;
     List<String> imageList;
-    LayoutInflater inflater;
 
     public ImageListAdapter(Context context, List<String> imageList) {
         this.context = context;
         this.imageList = imageList;
-        inflater = (LayoutInflater.from(context));
+    }
+
+    @NonNull
+    @Override
+    public ImageItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.image_list_adapter, parent, false);
+        return new ImageItemViewHolder(view);
     }
 
     @Override
-    public int getCount() {
-        return imageList.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
+    public void onBindViewHolder(@NonNull ImageItemViewHolder holder, int position) {
+        final Uri imageUri = Uri.parse(imageList.get(position));
+        try {
+            final Bitmap selectedImage =  getBitmapFormUri(context, imageUri);
+            System.out.println(selectedImage.getByteCount());
+            holder.imageView.setImageBitmap(selectedImage);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -47,31 +64,59 @@ public class ImageListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
-        ImageViewHolder holder;
-        if(view==null)
-        {
-            view=inflater.inflate(R.layout.image_list_adapter,null);
-            holder=new ImageViewHolder();
-            holder.subImgView=view.findViewById(R.id.subImageView);
-            view.setTag(holder);
-        }
-        else {
-            holder = (ImageViewHolder) view.getTag();
-        }
-        final Uri imageUri = Uri.parse(imageList.get(position));
-        final InputStream imageStream;
-        try {
-            imageStream = context.getContentResolver().openInputStream(imageUri);
-            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-            holder.subImgView.setImageBitmap(selectedImage);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return view;
+    public int getItemCount() {
+        return imageList.size();
     }
 
-    static class ImageViewHolder{
-        private ImageView subImgView;
+    public static Bitmap getBitmapFormUri(Context ac, Uri uri) throws FileNotFoundException, IOException {
+        InputStream input = ac.getContentResolver().openInputStream(uri);
+        BitmapFactory.Options onlyBoundsOptions = new BitmapFactory.Options();
+        onlyBoundsOptions.inJustDecodeBounds = true;
+        onlyBoundsOptions.inDither = true;//optional
+        onlyBoundsOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;//optional
+        BitmapFactory.decodeStream(input, null, onlyBoundsOptions);
+        input.close();
+        int originalWidth = onlyBoundsOptions.outWidth;
+        int originalHeight = onlyBoundsOptions.outHeight;
+        if ((originalWidth == -1) || (originalHeight == -1))
+            return null;
+        //Image resolution is based on 480x800
+        float hh = 800f;//The height is set as 800f here
+        float ww = 480f;//Set the width here to 480f
+        //Zoom ratio. Because it is a fixed scale, only one data of height or width is used for calculation
+        int be = 1;//be=1 means no scaling
+        if (originalWidth > originalHeight && originalWidth > ww) {//If the width is large, scale according to the fixed size of the width
+            be = (int) (originalWidth / ww);
+        } else if (originalWidth < originalHeight && originalHeight > hh) {//If the height is high, scale according to the fixed size of the width
+            be = (int) (originalHeight / hh);
+        }
+        if (be <= 0)
+            be = 1;
+        //Proportional compression
+        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+        bitmapOptions.inSampleSize = be;//Set scaling
+        bitmapOptions.inDither = true;//optional
+        bitmapOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;//optional
+        input = ac.getContentResolver().openInputStream(uri);
+        Bitmap bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions);
+        input.close();
+
+        return compressImage(bitmap);//Mass compression again
+    }
+
+    public static Bitmap compressImage(Bitmap image) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//Quality compression method, here 100 means no compression, store the compressed data in the BIOS
+        int options = 100;
+        while (baos.toByteArray().length / 1024 > 100) {  //Cycle to determine if the compressed image is greater than 100kb, greater than continue compression
+            baos.reset();//Reset the BIOS to clear it
+            //First parameter: picture format, second parameter: picture quality, 100 is the highest, 0 is the worst, third parameter: save the compressed data stream
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);//Here, the compression options are used to store the compressed data in the BIOS
+            options -= 10;//10 less each time
+        }
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());//Store the compressed data in ByteArrayInputStream
+        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);//Generate image from ByteArrayInputStream data
+        return bitmap;
     }
 }
